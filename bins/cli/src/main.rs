517761dd94fbe1b28b021b41db9c970a595dce90
Stefan Kuhn <semptic@kuhn.cloud>
2021-12-02 15:05:22 +0100

    Extract setup into its own function and remove lazy_static

--- bins/cli/src/main.rs
+++ bins/cli/src/main.rs
@@ -1,5 +1,4 @@
 #[macro_use]
-extern crate lazy_static;
 extern crate ansi_term;
 extern crate clap_verbosity_flag;
 extern crate loggerv;
@@ -12,30 +11,34 @@ use {{crate_name}}_lib_b::add;
 use structopt::StructOpt;
 
 fn main() -> Result<()> {
-    lazy_static! {
-        static ref OPT: Opt = Opt::from_args();
-    }
+    let opt = Opt::from_args();
 
+    setup(&opt).context("Failed to setup application environment")?;
+
+    hello(opt.name.as_str()).context("Failed to say hello")?;
+
+    info!("1 + 1 = {}", add(1, 1).context("Failed to add 1 and 1")?);
+
+    Ok(())
+}
+
+fn setup(opt: &Opt) -> Result<()> {
     #[cfg(windows)]
-    ansi_term::enable_ansi_support().unwrap();
+    ansi_term::enable_ansi_support().context("Failed to enable ansi support")?;
 
     loggerv::Logger::new()
         .max_level(
-            OPT.verbosity
+            opt.verbosity
                 .log_level()
                 .context("Failed to get log level")?,
         )
-        .level(OPT.debug)
-        .module_path(OPT.debug)
-        .line_numbers(OPT.debug)
+        .level(opt.debug)
+        .module_path(opt.debug)
+        .line_numbers(opt.debug)
         .init()
-        .unwrap();
+        .context("Failed to setup logger")?;
 
-    debug!("{:#?}", *OPT);
-
-    hello(OPT.name.as_str()).context("Failed to say hello")?;
-
-    info!("1 + 1 = {}", add(1, 1).context("Failed to add 1 and 1")?);
+    debug!("{:#?}", *opt);
 
     Ok(())
 }
