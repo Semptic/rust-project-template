f2d7d199f972e82469f72b529f4033d103ef4bed
Stefan Kuhn <semptic@kuhn.cloud>
2023-01-09 21:14:38 +0100

    Replace cargo make with shell script (#4)
    * Replace cargo make with shell script
    
    * Use correct crate name
    
    * Correctly install clippy
    
    * Correctly install fmt
    
    * Not use tool-cache since it's deprecated
    
    * Add debug messages
    
    * Use pre packaged cargo make
    
    * Try to make test script work
    
    * Use tmp dir
    
    * Use other install method again
    
    * Try to make cargo generate work
    
    * More debug stuff
    
    * Get abs path of script dir
    
    * Remove debug stuff
    
    * Use prebuild cargo make
    
    * Revert "Use prebuild cargo make"
    
    This reverts commit 090c683f377792c4bb2916be49ed27f8f3575d2f.

---
+++ test-template.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -e
+
+TOOLCHAIN='stable'
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TEST_TARGET_DIR=$(mktemp -d)
+
+function setup() {
+  mkdir -p $TEST_TARGET_DIR
+}
+
+function cleanup() {
+  rm -rf $TEST_TARGET_DIR
+}
+
+trap cleanup EXIT
+
+setup
+
+cd $TEST_TARGET_DIR
+
+cargo +$TOOLCHAIN generate --init --path $SCRIPT_DIR -n test_project -d description="This is a test for the cli template"
+
+# Make sure correct reamdme is produced
+if ! grep -Fxq "# test-project" README.md; then
+  echo "ERROR README.md missing the project title"
+  exit 1
+fi
+if ! grep -Fxq "This is a test for the cli template" README.md; then
+  echo "ERROR README.md is missing the project description"
+  exit 1
+fi
+
+# Make sure the correct dependabot config exists
+if [ ! -f ".github/dependabot.yml" ]; then
+  echo "ERROR .github/dependabot.yml is missing"
+  exit 1
+fi
+
+# Make sure github workflows configured correctly
+if [ -d ".github/workflows" ]; then
+  echo "ERROR .github/workflows should not exist"
+  exit 1
+fi
+
+cargo +$TOOLCHAIN make check-format
+cargo +$TOOLCHAIN make clippy
+cargo +$TOOLCHAIN make test
