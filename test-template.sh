cd26bccd26971ab66790d96cd32ad97e2f556f25
Stefan Kuhn <semptic@kuhn.cloud>
2023-01-10 10:21:13 +0100

    Add cargo outdated to check dependency updates

--- test-template.sh
+++ test-template.sh
@@ -7,6 +7,8 @@ TOOLCHAIN='stable'
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TEST_TARGET_DIR=$(mktemp -d)
 
+echo $TEST_TARGET_DIR
+
 function setup() {
   mkdir -p $TEST_TARGET_DIR
 }
@@ -26,3 +28,4 @@ cargo +$TOOLCHAIN generate --init --path $SCRIPT_DIR -n test_project -d descript
 cargo +$TOOLCHAIN make check-format
 cargo +$TOOLCHAIN make clippy
 cargo +$TOOLCHAIN make test
+cargo +$TOOLCHAIN outdated --exit-code 1
